92119a62f77b7eb757d86c3a66a9cdbfe3cb8dbf
tdelpech <thierry.delpech@soprasteria.com>
2023-07-28 16:10:54 +0200

    update

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,2 +1,7 @@
-var message = "Hello World";
-console.log(message);
+//let message : string = "Hello World !!!";
+function addition(x, y) {
+    return x + y;
+}
+var result = addition(20, 20);
+//console.log(message);
+console.log(addition(20, 20));
