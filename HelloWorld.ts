f8e01fbd204f4796635c14143dd97c79bcbab28b
tdelpech <thierry.delpech@soprasteria.com>
2023-07-28 12:15:16 +0200

    update helloworld.ts

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -3,6 +3,6 @@
 function addition(x, y) {
     return x + y;
 }
-const result = addition('10', '20');
+const result = addition(10, 20);
 //console.log(message);
-console.log(addition('10','20'));
+console.log(addition(10,20));
