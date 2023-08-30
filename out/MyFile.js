346d0c7d445ed38c6152e9e5060f78cd1807bae8
tdelpech <thierry.delpech@soprasteria.com>
2023-08-30 12:17:34 +0200

    new packages ts-node

---
+++ out/MyFile.js
@@ -0,0 +1,8 @@
+//const age: number = 10;
+//console.log(age);
+function substraction(x, y) {
+    return x - y;
+}
+const resultsub = substraction(30, 20);
+console.log(resultsub);
+//# sourceMappingURL=MyFile.js.map
---
+++ out/MyFile2.js
@@ -0,0 +1,6 @@
+function damage(CharacterToDammage, amount) {
+    CharacterToDammage.life = amount;
+    return CharacterToDammage.life;
+}
+const resultLife = damage({ life: 50 }, 12);
+//# sourceMappingURL=MyFile2.js.map
