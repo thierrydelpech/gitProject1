a517c666589034c1ca16f299079d949d06b45519
tdelpech <thierry.delpech@soprasteria.com>
2023-09-13 15:02:25 +0200

    update MyFile3.ts

--- MyFile3.js
+++ MyFile3.js
@@ -1,4 +1,25 @@
-function sayHello(target) {
-    return "Hello ".concat(target.firstname, ", you have ").concat(target.money, " on your bank account.");
+/*function sayHello(target:{firstname: string, money:number}){
+    return `Hello ${target.firstname}, you have ${target.money} on your bank account.`;
 }
-var message = sayHello({ firstname: 'thierry', money: 123 });
+const message: string = sayHello({ firstname: 'thierry', money: 5000 });
+console.log(message);*/
+var Car = /** @class */ (function () {
+    function Car(engine, marque) {
+        this.engine = engine;
+        this.marque = marque;
+        this.Engine = engine;
+        this.Marque = marque;
+    }
+    Car.prototype.DisplayCarEngine = function () {
+        console.log(this.GetCarEngine());
+    };
+    Car.prototype.GetCarEngine = function () {
+        return 'The engine is: ' + this.engine;
+    };
+    return Car;
+}());
+//let bigCar = new Car('V8','Citroen'); 
+var littleCar = new Car('2CV', 'Peugeot');
+//console.log(littleCar.Engine, littleCar.Marque); 
+var carEngine = littleCar.GetCarEngine();
+console.log(littleCar.GetCarEngine());
